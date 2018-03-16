380923cb00fdd108b37118263e3303dc95bd0c8c
Daniel Winkler <winkler.daniel1@gmail.com>
2018-03-16 20:32:08 +0100

    Added script to detect uninstalled libraries

---
+++ getPackage.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+from subprocess import check_output
+
+installed = str(check_output(["R", "-e installed.packages()[,1]"]))
+installed = re.findall('"[A-Za-z0-9]*"', installed)
+installed = [name.strip('"') for name in installed]
+
+try:
+    sys.argv[1]
+except IndexError:
+    fname = str(input("Filename: "))
+else:
+    fname = str(sys.argv[1])
+    
+rfile = open(fname, 'r')
+ftext = rfile.read()
+rfile.close()
+rLibs = re.findall('(?<=library\()["\'A-Za-z0-9]*', ftext)
+rReq = re.findall('(?<=require\()["\'A-Za-z0-9]*', ftext)
+rLibs = rLibs + rReq
+rLibs = [name.strip("'") for name in rLibs]
+rLibs = [name.strip('"') for name in rLibs]
+rLibs = list(set(rLibs) - set(installed))
+
+command = 'install.packages("' + '","'.join(rLibs) + '")'
+print("Run: \n")
+print(command + "\n")
